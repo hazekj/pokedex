1a1db2973484e0e61e8028522788a24015ac3b02
hazekj <hazekj@gmail.com>
2024-09-30 19:18:17 -0600

    Add map and mapb commands

--- command_map.go
+++ command_map.go
@@ -1,5 +1,33 @@
 package main
 
-func commandMap() error {
+import (
+	"fmt"
+)
 
+func commandMap(cfg *config) error {
+	locations, err := cfg.pokeClient.ListLocations(cfg.NextURL)
+	if err != nil {
+		return err
+	}
+	cfg.PreviousURL = locations.Previous
+	cfg.NextURL = locations.Next
+	for i, loc := range locations.Results {
+		fmt.Printf("(%d) %s \n", i, loc.Name)
+	}
+
+	return nil
+
+}
+
+func commandMapBack(cfg *config) error {
+	locations, err := cfg.pokeClient.ListLocations(cfg.PreviousURL)
+	if err != nil {
+		return err
+	}
+	cfg.PreviousURL = locations.Previous
+	cfg.NextURL = locations.Next
+	for i, loc := range locations.Results {
+		fmt.Printf("(%d) %s \n", i, loc.Name)
+	}
+	return nil
 }
